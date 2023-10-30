e30445ccfd5bec562ad42a81ed68591807c13a9a
Naganathan05 <umaiyalramesh@gmail.com>
2023-10-30 14:58:14 +0530

    Python file updated

--- Practice 1.py
+++ Practice 1.py
@@ -7,4 +7,9 @@ s = (a + b + c)/2
 area = ((s*(s - a)*(s - b)*(s - c)) ** 0.5)
 print(area)
 
+if a % 2 == 0:
+    print(a," Is a even number.")
+else:
+    print(a," Is a odd number.")
+
 #My Comment 
